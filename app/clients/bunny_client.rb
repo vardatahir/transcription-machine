875a5d6b5a38ea649e5aa9e388bf4259dcea5752
Varda Tahir <vardatahir@gmail.com>
2025-02-25 13:03:23 +0500

    Syncing Videos

--- app/clients/bunny_client.rb
+++ app/clients/bunny_client.rb
@@ -7,6 +7,10 @@ class BunnyClient
     @library_id, @access_key = library_id, access_key
   end
 
+  def videos(page: 1, per_page: 1000)
+    get "/videos?itemsPerPage=#{per_page}&page=#{page}"
+  end
+
   def get(path)
     http_request Net::HTTP::Get, "/library/#{@library_id}#{path}"
   end
@@ -14,6 +18,7 @@ class BunnyClient
   def post(path, body:)
     http_request Net::HTTP::Post, "/library/#{@library_id}#{path}", body: body
   end
+end
 
   private
 
@@ -21,7 +26,7 @@ class BunnyClient
   def http_request(method, path, body: nil)
     uri = URI("#{BASE_URI}#{path}")
 
-    http = Net::HTTP:.new(uri.host, uri.port)
+    http = Net::HTTP::new(uri.host, uri.port)
     http.use_ssl = uri.is_a?(URI::HTTPS)
 
     request = method.new(uri)
--- app/integrations/bunny.rb
+++ app/integrations/bunny.rb
@@ -1,4 +1,33 @@
 class Bunny
-  def sync
+  attr_reader :client
+
+  def initialize(access_key: nil, library_id: nil)
+    access_key ||= Rails.application.credentials.bunny_access_key
+    library_id ||= Rails.application.credentials.bunny_library_id
+
+    @client = BunnyClient.new(access_key: access_key, library_id: library_id)
+  end
+
+
+  def sync(page: 1, per_page: 1000)
+    loop do
+      response = client.videos(page: page, per_page: per_page)
+      Video.transaction do
+        response[:items].each{ sync_video(_1) }
+      end
+      next_page = response[:currentPage] * response[:itemsPerPage] < response[:totalItems]
+      break unless next_page
+      page += 1
+    end
+  end
+
+
+  def sync_video(item)
+    video = Video.where(guid: item[:guid]).first_or_initialize
+    video.update(
+      library_id: item[:videoLibraryId],
+      title: item[:title],
+      captions: item[:captions].any?{ _1[:label] == "English" }
+    )
   end
 end
